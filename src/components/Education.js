e0348ebb55f17ba617797f57e871aabd51d6fda9
admin <admin@admins-MBP.fios-router.home>
2018-02-01 08:04:44 -0500

    Update education

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,23 +1,18 @@
 import React from "react";
+import Small from "../elements/Small";
+
+const smallText = `One of the things I am most passionate about is learning. I feel strongly that everyone should 
+aspire to be a lifelong learner. In my free time, I like to expand my knowledge by reading and
+taking online courses. I try to learn something new every day.`
 
 const Education = () => (
   <div>
-    <h2>📝 Latest Blog Posts</h2>
+    <h2>Education</h2>
+    <Small text={smallText}/>
     <ul style={{ listStyle: "none" }}>
-      <li>
-        - Some Blog Post Title&nbsp;
-        <a href="#">(Read)</a>
-      </li>
-      <li>
-        - Some Blog Post Title&nbsp;
-        <a href="#">(Read)</a>
-      </li>
-      <li>
-        - Gatsby.js: How to set up and use the React Static Site Generator&nbsp;
-        <a href="https://medium.freecodecamp.org/setting-up-and-getting-used-to-gatsby-1fc27985ae8a">
-          (Read)
-        </a>
-      </li>
+      <li><strong>React Nanodegree</strong> - Udacity</li>
+      <li><strong>Frontend Web Developer Nanodegree</strong> - Udacity</li>
+      <li><strong>Bachelor of Fine Arts</strong> - The University of the Arts</li>
     </ul>
   </div>
 );
