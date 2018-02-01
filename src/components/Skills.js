d81a21bba28e7ebf6ce72315e97b5b3e0684a5d8
admin <admin@admins-MacBook-Pro.local>
2018-02-01 07:47:54 -0500

    Update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const Skills = () => (
   <div>
-    <h2>✅ Skills</h2>
-    <p>👩‍💻 Code</p>
+    <h2>Skills</h2>
+    <ul style={{listStyle: "none"}}>
+      <li>JavaScript: ES5 - ES2015+, jQuery</li>
+      <li>React ecosystem: React, React Native, Redux</li>
+      <li>Frontend development: JS, CSS, HTML, Bootstrap</li>
+      <li>Backend: LAMP stack - CentOS/Ubuntu, Apache, MySQL, PHP</li>
+      <li>Version control: Git, GitLab, GitHub</li>
+      <li>Backend services: Netlify, Graph.cool</li>
+      <li>Other: Babel, Webpack, Rollup, Jest, Jasmine, Bash</li>
+    </ul>
   </div>
 );
 
