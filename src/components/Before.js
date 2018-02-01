0bef3895300a02c4f9fdacb0bb8635d892e9049d
admin <admin@admins-MacBook-Pro.local>
2018-02-01 07:36:27 -0500

    update work experience

--- src/components/Before.js
+++ src/components/Before.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const Before = () => (
   <div>
-    <h2>⏳️ Before</h2>
-    ABc inc, Xyz Tech Inc
+    <h2>Work Experience</h2>
+    <ul style={{listStyle: "none"}}>
+      <li>2015 - Present: <strong>Full-Stack Web Developer</strong>, IntuitSolutions</li>
+      <li>2013 - 2015: <strong>Online Auction Director</strong>, Great Dane Auctions</li>
+      <li>2010 - 2013: <strong>Gallery Director</strong>, I. Brewster &amp; Sons Gallery</li>
+      <li>2010: <strong>Marketing &amp; Programming Intern</strong>, Philagrafika</li>
+    </ul>
   </div>
 );
 
